20d78751b0cbaa0e376fc360896126cd2dbe681b
Kholifatulardhi08 <ardhi.kholifatul8@gmail.com>
2022-09-01 22:50:09 +0700

    add delete function controller
    Signed-off-by: Kholifatulardhi08 <ardhi.kholifatul8@gmail.com>

--- app/Http/Controllers/deleteController.php
+++ app/Http/Controllers/deleteController.php
@@ -6,6 +6,7 @@ use App\Models\Unit;
 use App\Models\Room;
 use App\Models\Drink;
 use App\Models\Meal;
+use App\Models\Booking;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
@@ -36,6 +37,13 @@ class deleteController extends Controller
         }
         else if ($req->type === 'Meal'){
             $data = Meal::where('id', $req->id)->first();
+            $data->status_active = false;
+            $data->save();
+
+            return response()->json(['data' => $data], Response::HTTP_OK);
+        }
+        else if ($req->type === 'Booking'){
+            $data = Booking::where('id', $req->id)->first();
             $data->status_active = false;
             $data->save();
 
